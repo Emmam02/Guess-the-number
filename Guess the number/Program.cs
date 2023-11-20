ac5cf5db053e031f3039b5d9ab403c265fb14112
Emmam02 <135839715+Emmam02@users.noreply.github.com>
2023-11-20 19:38:34 +0100

    Initial commit

---
+++ Guess the number/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+
+namespace GuessTheNumber
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randomNumber = new Random();
+            int secretNumber = randomNumber.Next(1, 101);
+            int numberGuessed = 0;
+     
+
+            do
+            {
+                Console.WriteLine("Enter a number between 1 and 100 :");
+                numberGuessed = Convert.ToInt32(Console.ReadLine());
+                if (secretNumber < numberGuessed)
+                {
+                    Console.WriteLine("Go lower");
+                }
+                else if (secretNumber > numberGuessed)
+                {
+                    Console.WriteLine("Go higher");
+                }
+            } while (secretNumber != numberGuessed);
+
+            Console.WriteLine("You guessed it, it was {0}!", secretNumber);
+        }
+    }
+}
